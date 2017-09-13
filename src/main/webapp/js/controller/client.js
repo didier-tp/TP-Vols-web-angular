3ca494e87d1eddb33feb9596fb8a0b56259067f0
LeroyJeremy <jrmleroy@wanadoo.fr>
2017-09-13 17:01:02 +0200

    clientDifferentTermine

--- src/main/webapp/js/controller/client.js
+++ src/main/webapp/js/controller/client.js
@@ -11,6 +11,16 @@
 				self.clients = [];
 			
 				self.client = null;
+				
+				self.type = null;
+
+				self.setType = function(val) {
+					self.type = val;
+				};
+
+				self.isType = function(val) {
+					return this.type === val;
+				};
 			
 				self.list = function() {
 					$http({
@@ -22,8 +32,21 @@
 					});
 				};
 				
-				self.add = function() {
+				
+				
+				self.addClientPhysique = function() {
 					this.client = {};
+					self.setType('Physique');
+				};
+				
+				self.addClientMoral = function() {
+					this.client = {};
+					self.setType('Moral');
+				};
+				
+				self.addClientEI = function() {
+					this.client = {};
+					self.setType('EI');
 				};
 			
 				self.edit = function(id) {
@@ -32,15 +55,22 @@
 						url : 'api/client/'+id
 					}).then(function(response) {
 						self.client = response.data;
+						if (self.client.type ==="ClientPhysique"){
+							self.setType('Physique');
+						}else if(self.client.type ==="ClientMoral"){
+							self.setType('Moral');
+						}else if(self.client.type ==="ClientEI"){
+							self.setType('EI');
+						}
 					}, function(response) {
 					});
 				};
 			
-				self.save = function() {
+				self.saveClientPhysique = function() {
 					if(self.client.id !=null) {
 						$http({
 							method : 'PUT',
-							url : 'api/client/'+self.client.id,
+							url : 'api/clientPhysique/'+self.client.id,
 							data : self.client
 						}).then(function(response) {
 							self.cancel();
@@ -51,7 +81,57 @@
 						console.log(self.client);
 						$http({
 							method : 'POST',
-							url : 'api/client/',
+							url : 'api/clientPhysique/',
+							data : self.client
+						}).then(function(response) {
+							self.cancel();
+							self.list();
+						}, function(response) {
+						});
+					}
+				};
+				
+				self.saveClientMoral = function() {
+					if(self.client.id !=null) {
+						$http({
+							method : 'PUT',
+							url : 'api/clientMoral/'+self.client.id,
+							data : self.client
+						}).then(function(response) {
+							self.cancel();
+							self.list();
+						}, function(response) {
+						});
+					} else {
+						console.log(self.client);
+						$http({
+							method : 'POST',
+							url : 'api/clientMoral/',
+							data : self.client
+						}).then(function(response) {
+							self.cancel();
+							self.list();
+						}, function(response) {
+						});
+					}
+				};
+				
+				self.saveClientEI = function() {
+					if(self.client.id !=null) {
+						$http({
+							method : 'PUT',
+							url : 'api/clientEI/'+self.client.id,
+							data : self.client
+						}).then(function(response) {
+							self.cancel();
+							self.list();
+						}, function(response) {
+						});
+					} else {
+						console.log(self.client);
+						$http({
+							method : 'POST',
+							url : 'api/clientEI/',
 							data : self.client
 						}).then(function(response) {
 							self.cancel();
@@ -74,6 +154,7 @@
 			
 				self.cancel = function() {
 					self.client = null;
+					self.setType(null);
 					self.clientForm.$setPristine();
 				};
 				
