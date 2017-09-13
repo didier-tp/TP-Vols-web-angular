e293633fdeddd75ce084f099516c93d99e548e3f
dedeledindon <exemple@gmail.com>
2017-09-13 14:33:49 +0200

    matt

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,18 +1,41 @@
-(function () {
-    var app = angular.module("PremierPas", []);
+(function() {
+	var app = angular.module("PremierPas",
+			[ "Login", "Client", "Vol", "Aeroport", "CompagnieAerienne",
+					"Passager", "Reservation", "Ville" ]);
 
-    app.controller("HomeController", function ($http) {
-        self = this;
-        self.tab = "home";
+	app.controller("HomeController", function($http) {
+		self = this;
+		self.tab = "home";
 
-        self.setTab = function (val) {
-            self.tab = val;
-        };
+		self.setTab = function(val) {
+			self.tab = val;
+		};
 
-        self.isTab = function (val) {
-            return this.tab === val;
-        };
+		self.isTab = function(val) {
+			return this.tab === val;
+		};
 
+		self.listTitrePhysique = function() {
+			$http({
+				method : 'GET',
+				url : 'api/titrePhysique'
+			}).then(function(response) {
+				self.titrePhysique = response.data;
+			}, function(response) {
+			});
+		};
 
-    });
+		self.listTitreMoral = function() {
+			$http({
+				method : 'GET',
+				url : 'api/titreMoral'
+			}).then(function(response) {
+				self.titreMoral = response.data;
+			}, function(response) {
+			});
+		};
+		self.listTitrePhysique();
+		self.listTitreMoral();
+
+	});
 })();
