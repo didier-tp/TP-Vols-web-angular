bb154b7ff8e8c06fb40818e2975507a6eaa05f12
oumzai <oumzai-chahrazade@yahoo.fr>
2017-09-13 12:07:04 +0200

    compagnie

---
+++ src/main/java/formation/web/CompagnieAerienneRestController.java
@@ -0,0 +1,82 @@
+package formation.web;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import vol.metier.dao.CompagnieAerienneDao;
+import vol.metier.model.CompagnieAerienne;
+import vol.metier.model.Views;
+
+@RestController
+public class CompagnieAerienneRestController {
+
+	@Autowired
+	private CompagnieAerienneDao compagnieAerienneDao;
+
+	@RequestMapping(value = "/compagnieAerienne", method = RequestMethod.GET)
+	@JsonView(Views.CompagnieAerienne.class)
+	public ResponseEntity<List<CompagnieAerienne>> list() {
+		return new ResponseEntity<>(compagnieAerienneDao.findAll(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/compagnieAerienne", method = RequestMethod.POST)
+	public ResponseEntity<Void> create(@RequestBody CompagnieAerienne compagnieAerienne, UriComponentsBuilder uCB) {
+		CompagnieAerienne tmp = compagnieAerienneDao.find(compagnieAerienne.getId());
+		if (tmp == null) {
+			compagnieAerienneDao.create(compagnieAerienne);
+			URI uri = uCB.path("/compagnieAerienne/{id}").buildAndExpand(compagnieAerienne.getId()).toUri();
+			HttpHeaders header = new HttpHeaders();
+			header.setLocation(uri);
+			return new ResponseEntity<>(header, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		}
+	}
+
+	@RequestMapping(value = "/compagnieAerienne/{id}", method = RequestMethod.GET)
+	@JsonView(Views.CompagnieAerienne.class)
+	public ResponseEntity<CompagnieAerienne> find(@PathVariable("id") Long id) {
+		CompagnieAerienne tmp = compagnieAerienneDao.find(id);
+		if (tmp == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<CompagnieAerienne>(tmp, HttpStatus.OK);
+		}
+	}
+
+	@RequestMapping(value = "/compagnieAerienne/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+		CompagnieAerienne tmp = compagnieAerienneDao.find(id);
+		if (tmp == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			compagnieAerienneDao.delete(tmp.getId());
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+	}
+
+	@RequestMapping(value = "/compagnieAerienne/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable("id") Long id, @RequestBody CompagnieAerienne compagnieAerienne) {
+		CompagnieAerienne tmp = compagnieAerienneDao.find(id);
+		if (tmp == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			compagnieAerienneDao.update(compagnieAerienne);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+	}
+
+}
