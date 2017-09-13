39eb085bd14f9f17f990dd63d7f21d00c2990ee1
Alexandre Le Jeune <alexandre.lejeune@soprabanking.com>
2017-09-13 10:41:36 +0200

    rest vol

---
+++ src/main/java/formation/web/VolRestController.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package formation.web;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import vol.metier.dao.VolDao;
+import vol.metier.model.Vol;
+
+/**
+ * @author ajc
+ *
+ */
+@RestController
+public class VolRestController {
+
+	@Autowired
+	private VolDao daoVol;
+
+	@RequestMapping(value = "/vol", method = RequestMethod.GET)
+	public ResponseEntity<List<Vol>> list() {
+		return new ResponseEntity<>(daoVol.findAll(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/vol", method = RequestMethod.POST)
+	public ResponseEntity<Void> create(@RequestBody Vol vol, UriComponentsBuilder uCB) {
+		daoVol.create(vol);
+		URI uri = uCB.path("/vol/{id}").buildAndExpand(vol.getId()).toUri();
+		HttpHeaders header = new HttpHeaders();
+		header.setLocation(uri);
+		return new ResponseEntity<>(header, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/vol/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Vol> find(@PathVariable("id") Long id) {
+		Vol tmp = daoVol.find(id);
+		if (tmp == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<Vol>(tmp, HttpStatus.OK);
+		}
+	}
+
+	@RequestMapping(value = "/vol/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+		Vol tmp = daoVol.find(id);
+		if (tmp == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			daoVol.delete(tmp);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+	}
+
+	@RequestMapping(value = "/vol/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable("id") Long id, @RequestBody Vol vol) {
+		Vol tmp = daoVol.find(id);
+		if (tmp == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			daoVol.update(tmp);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+	}
+
+}
